e7d74883a5d9a05a7b4602064c18cafc78c26c94
DESKTOP-7ASJGVK\Sabina <monaldinimarco@gmail.com>
2025-01-27 15:02:23 +0100

    Normalizzazione dei finali di riga

---
+++ CasaLeo/src/test/java/com/leo/CasaLeo/CasaLeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.CasaLeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaLeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
