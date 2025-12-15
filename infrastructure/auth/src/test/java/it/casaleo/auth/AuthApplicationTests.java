0bb16b10ac8baadbd53da53ee02bd4aa961efbac
DESKTOP-7ASJGVK\Sabina <monaldinimarco@gmail.com>
2025-12-15 15:44:20 +0100

    Creazione del servizio dedicato all'autenticazione.

---
+++ infrastructure/auth/src/test/java/it/casaleo/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.casaleo.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
